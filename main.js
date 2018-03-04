839b9e6653a9ec71df0502d7156837e5eaede17f
Jarek Figura <jarek.figura@icloud.com>
2018-03-04 15:19:44 +0100

    Level 2

--- main.js
+++ main.js
@@ -3,4 +3,14 @@ jQuery(document).ready(function () {
 //	$("span").text("$100");
 //	$(".vacation h2").text("Gdansk, Poland"); // 1.11
 //	$("#vacations .america");                 // 1.13
+//	$("#tours > li");       // 2.4
+//	$(".asia, .sale");      // 2.5
+//	$("#tours li:first");   // 2.6
+//	$("#tours > li:even");  // 2.7
+//	$("#vacations").find(".america");   // 2.9
+//	$("#vacations li").first();         // 2.10
+//	$("#vacations li").last();          // 2.11
+//	$("#vacations li").last().prev();   // 2.12
+//	$("#tours .featured").parent();     // 2.13
+//	$("#tours").children("li");         // 2.14
 });
