1711a066cb79760d77d5e87ea371ed3d81cf660f
Jarek Figura <jarek.figura@icloud.com>
2018-03-04 14:31:09 +0100

    Level 1

---
+++ main.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function () {
+//	$("h2");
+//	$("span").text("$100");
+//	$(".vacation h2").text("Gdansk, Poland"); // 1.11
+//	$("#vacations .america");                 // 1.13
+});
