93e5c701e18dcc5b081140f521dfe6821d0fa897
Jarek Figura <jarek.figura@icloud.com>
2018-03-05 15:04:48 +0100

    Level 4.20

--- main.js
+++ main.js
@@ -30,8 +30,12 @@ $(document).ready(function () {
 //	});
 
 // LEVEL 4
-	$('.see-photos').on('click', function () {
+	$('.see-photos').on('click', function (event) {
+		event.stopPropagation();
 		$(this).closest('.tour').find('.photos').slideToggle();
+	});
+	$('.tour').on('click', function() {
+		alert('This event handler should not be called.');
 	});
 
 });
