fb4555e8ad0f98a9f659ce84a7f2c570049a9ea6
Jarek Figura <jarek.figura@icloud.com>
2018-03-05 11:39:36 +0100

    Level 3.13

--- main.js
+++ main.js
@@ -20,8 +20,8 @@ $(document).ready(function () {
 // LEVEL 3
 	$("button").on("click", function () {
 		var message = $('<span>Call 1-555-jquery-air to book this tour</span>');
-		$('.usa').append(message);
-		$('button').remove();
+		$(this).after(message);
+		$(this).remove();
 	});
 
 });
